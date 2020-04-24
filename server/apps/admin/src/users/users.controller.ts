b19c887b924884f33d7e23f91e7cdf9f1ff96d16
onlyhy <hyuestu@163.com>
2020-04-24 16:26:02 +0800

    第一章：4-使用swagger和装饰器编写接口文档

--- server/apps/admin/src/users/users.controller.ts
+++ server/apps/admin/src/users/users.controller.ts
@@ -2,13 +2,17 @@ import { Controller } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { User } from '@libs/db/models/user.model';
 import { Crud } from 'nestjs-mongoose-crud'
+import { ApiTags } from '@nestjs/swagger';
 
 @Crud({
-    model:User
+// 指定是哪个模型
+    model: User
 })
 @Controller('users')
+@ApiTags('用户')
 export class UsersController {
-    constructor(@InjectModel(User) private readonly model){
-
+    // 注入模型到model属性上
+    constructor(@InjectModel(User) private readonly model) {
+        
     }
 }
--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,12 +1,22 @@
 /*
  * @Date: 2020-04-22 16:44:36
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-04-22 16:46:01
+ * @LastEditTime: 2020-04-24 16:11:14
  */
-import { prop } from '@typegoose/typegoose'
+import { prop, modelOptions } from '@typegoose/typegoose'
+import { ApiProperty } from '@nestjs/swagger'
+
+@modelOptions({
+    schemaOptions:{
+        timestamps:true
+    }
+})
 export class User {
+    @ApiProperty({description:"用户名",example: 'user1'})
     @prop()
     username: string
+
+    @ApiProperty({ description: "密码",example:'pass1' })
     @prop()
     password: string
 }
