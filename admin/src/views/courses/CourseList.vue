07faeaa3adbcf1679f2460cc32f3cd3a4b654308
onlyhy <hyuestu@163.com>
2020-05-01 10:48:06 +0800

    第一章：10-带确认框的删除课程功能

--- admin/src/views/courses/CourseList.vue
+++ admin/src/views/courses/CourseList.vue
@@ -1,11 +1,14 @@
 <!--
  * @Date: 2020-04-28 14:08:29
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-04-29 16:44:21
+ * @LastEditTime: 2020-05-01 10:47:05
  -->
 <template>
   <div>
     <h3>课程列表</h3>
+    <div>
+      <el-button type="success" size="small" @click="$router.push('/courses/create')">创建课程</el-button>
+    </div>
     <el-table :data="data.data">
       <!-- 这里fields是一个对象，所以v-for里面的name代表的是键名 -->
       <el-table-column
@@ -14,11 +17,15 @@
         :key="name"
         :label="field.label"
         :width="field.width"
-      >
-      </el-table-column>
+      ></el-table-column>
       <el-table-column label="操作" :width="200">
         <template v-slot="{row}">
-          <el-button type="success" size="small" @click="$router.push(`/courses/edit/${row._id}`)">编辑</el-button>
+          <el-button
+            type="success"
+            size="small"
+            @click="$router.push(`/courses/edit/${row._id}`)"
+          >编辑</el-button>
+          <el-button type="danger" size="small" @click="remove(row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -40,6 +47,18 @@ export default class CourseList extends Vue {
   async fetch() {
     const res = await this.$http.get("courses");
     this.data = res.data;
+  }
+
+  async remove(row) {
+    try {
+      await this.$confirm("是否确认删除？");
+    } catch (e) {
+      // 中断方法的执行
+      return
+    }
+    await this.$http.delete(`courses/${row._id}`);
+    this.$message.success("删除成功");
+    this.fetch();
   }
   created() {
     this.fetch();
